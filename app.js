0a503eae1e0df635a2c774220549f8572cf66b86
Balaji-Nandure <110956558+Balaji-Nandure@users.noreply.github.com>
2022-12-17 12:32:14 +0530

    logic bulding

--- app.js
+++ app.js
@@ -6,21 +6,49 @@ const state = {
     earnings: 0,
     expenses: 0,
     net: 0,
-    transactions: [
-        {
-            id: 5,
-            text: "demo credit",
-            amount: 500,
-            type: "credit",
-        },
+    transactions: [],
+};
 
-        {
-            id: 4,
-            text: "demo debit",
-            amount: 400,
-            type: "debit",
-        },
-    ],
+const renderTransactions = () => {
+    const transactionsContainerEl =
+        document.getElementsByClassName("transactions");
+    const netAmountEl = document.getElementById("netAmount");
+    const earningEl = document.getElementById("earning");
+    const expenseEl = document.getElementById("expense");
+
+    const transactions = state.transactions;
+    transactionsContainerEl.innerHTML = "";
+
+    let earning = 0;
+    let expense = 0;
+    let net = 0;
+
+    transactions.forEach((transaction) => {
+        const { id, text, amount, type } = transaction;
+        const isCredit = type === "credit" ? true : false;
+        const sign = isCredit ? "+" : "-";
+
+        let transactionEl = `
+        <div class="transaction" id="${id}"> 
+            <div class="left">
+                <p>${text}</p>
+                <p>${sign} ${amount}</p>
+            </div>
+            <div class="status ${isCredit ? "credit" : "debit"}">${
+            isCredit ? "C" : "D"
+        }</div>
+        </div>`;
+
+        earning += isCredit ? amount : 0;
+        expense += isCredit ? 0 : amount;
+        net = earning - expense;
+
+        transactionsContainerEl.insertAdjacentHTML("afterbegin", transactionEl);
+    });
+
+    netAmountEl.innerText = `${net}`;
+    earningEl.innerText = `${earning}`;
+    expenseEl.innerText = `${expense}`;
 };
 
 const addTransaction = (e) => {
@@ -35,7 +63,20 @@ const addTransaction = (e) => {
         tData[key] = value;
     });
 
-    console.log(tData);
+    // console.log(tData);
+
+    const { text, amount } = tData;
+
+    const transaction = {
+        id: Math.floor(Math.random() * 1000),
+        text: text,
+        amount: +amount,
+        type: isEarn ? "credit" : "debit",
+    };
+
+    state.transactions.push(transaction);
+
+    renderTransactions();
 };
 
 transactionFormEl.addEventListener("submit", addTransaction);
